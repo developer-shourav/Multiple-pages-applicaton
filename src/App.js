f0c40e4336469ec5e5441f393cac6b16704fb434
Shourav Rajbongshi <mrinmoyraj823@gmail.com>
2022-03-08 21:12:45 +0600

    all the routing removed

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
 
-import { Switch } from "react-router-dom";
-import { Route } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-import About from "./components/About/About";
-import Friends from "./components/Friends/Friends";
-import Home from "./components/Home/Home";
-import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter> 
-      <Switch>
-        <Route exact path="/"> 
-          <Home> </Home>
-        </Route>
-
-        <Route path="/home"> 
-        <Home> </Home>
-        </Route>
-
-        <Route path="/friends">
-           <Friends> </Friends>
-         </Route>
-
-         <Route path="/about">
-           <About> </About>
-         </Route>
-
-         <Route path="*"> 
-           <NotFound> </NotFound>
-         </Route> 
-      </Switch> 
-      </BrowserRouter>
+     
     </div>
   );
 }
